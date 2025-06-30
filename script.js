17d75e484a3b231b185bf2dfcbfddb7a7b702f38
Hannah Edwards <edwards.hs@outlook.com>
2025-06-30 18:35:22 -0400

    Implementing Jquery
    Moving jquery to index

--- script.js
+++ script.js
@@ -4,32 +4,5 @@ function menuFunction(){
   m.style.opacity = 1
 }
 
-function servicesFunction(){
-  var s = document.getElementById("services")
-  s.style.description.hide
 
-  function showFullHeight() {
-    $(".gallery li").each(function () {
-      $(this)
-        .find(".btn")
-        .click(function (e) {
-          e.preventDefault();
 
-          //NO MATTER WHAT WE CLOSE ALL OPEN SLIDES
-          $(".description").slideUp("normal");
-
-          //IF THE NEXT SLIDE WASN'T OPEN THEN OPEN IT
-          if ($(this).next().is(":hidden") == true) {
-            //ADD THE ON CLASS TO THE BUTTON
-            $(this).addClass("on");
-
-            //OPEN THE SLIDE
-            $(this).next().slideDown("normal");
-          }
-        }); //click
-    }); //each
-  } //function
-
-  showFullHeight();
-
-};
